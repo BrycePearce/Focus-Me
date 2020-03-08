fceb76313ef95574907db0e2cbc7c2121daf990f
bpearce@angelo.edu <bpearce@angelo.edu>
2020-03-08 03:19:56 -0500

    add to blocklist functionality, redirect page. Several todos, but working at this point.

--- src/popout/popout.js
+++ src/popout/popout.js
@@ -1,15 +1,24 @@
-let extensionToggle = document.getElementById('enable-site-blocking');
-
 // Set initial state whenever popout is loaded, based on storage
 // todo: fix bug where opening popout shows animation to set the current state
 chrome.storage.sync.get('isExtensionActive', storage => {
     extensionToggle.checked = storage.isExtensionActive;
 });
 
-// listen for a toggle event
-extensionToggle.onclick = setToggleState;
+document.addEventListener(('DOMContentLoaded'), () => {
+    setDOMValues();
+});
 
-// Update the state
+/* DOM Elements */
+let extensionToggle = document.getElementById('enable-site-blocking');
+let addBlockedButton = document.getElementById('blocklist');
+
+
+/* Event Listeners */
+extensionToggle.onclick = setToggleState;
+addBlockedButton.onclick = addToBlockList;
+
+/* Methods */
+// Update the extension active state
 function setToggleState() {
     chrome.storage.sync.get('isExtensionActive', storage => {
         extensionToggle.checked = !storage.isExtensionActive;
@@ -17,4 +26,48 @@ function setToggleState() {
             isExtensionActive: !storage.isExtensionActive,
         });
     });
+}
+
+// todo: refactor tabs into an async method, if possible? Using here and in addToBlockList
+function setDOMValues() {
+    chrome.tabs.query({
+        'active': true,
+        'currentWindow': true
+    }, (tabs) => {
+        // todo: handle invalid URL's
+        const currentlyActiveUrl = tabs[0].url;
+        document.getElementById('blocklist').textContent = `Block ${getDomainFromUrl(currentlyActiveUrl)}?`
+    });
+}
+
+function addToBlockList() {
+    // fetch the current blocklist
+    chrome.storage.sync.get(['blocklist'], (storage) => {
+        let blockList = storage['blocklist'] ? storage['blocklist'] : new Array();
+        // get the current tab, append the current tab URL to our blocklist
+        chrome.tabs.query({
+            'active': true,
+            'currentWindow': true
+        }, (tabs) => {
+            const currentlyActiveUrl = tabs[0] ? getDomainFromUrl(tabs[0].url) : null;
+            if (currentlyActiveUrl && !blockList.includes(currentlyActiveUrl)) {
+                blockList.push(currentlyActiveUrl);
+                // update our storage with the updated blocklist
+                chrome.storage.sync.set({
+                    'blocklist': blockList
+                });
+            }
+        });
+    });
+}
+
+function getDomainFromUrl(url) {
+    const urlRegex = new RegExp(/(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/, 'g');
+    const isValidUrl = urlRegex.test(url);
+    if (isValidUrl) {
+        // https://www.google.com/hello -> google.com
+        return new URL(url).hostname.replace(/^(?:https?:\/\/)?(?:www\.)?/i, "").split('/')[0];
+    } else {
+        return undefined;
+    }
 }
--- src/site-blocker.js
+++ src/site-blocker.js
@@ -1,19 +1,50 @@
 // Content Script
 // Handles reading data of the pages the user visits, and passing information to the extension. This is where we orchestrate actions based on events.
+// *note: this script will run when a tab is initialized
 // https://developer.chrome.com/extensions/content_scripts
 
-// Extension loaded on a new page
+// Extension loaded on a new page, check to see if it needs to be blocked
 chrome.storage.sync.get('isExtensionActive', storage => {
-    handleToggleEvent(storage.isExtensionActive);
+    blockWebsite(storage.isExtensionActive);
 });
 
-// Status is changed on the current tab
+// Extension Storage update event
+/*
+ * todo:
+ * Listening for storage updates is probably not the best way to handle this, since it requires re-fetching all important information again, everytime.
+ * Consider the following solutions:
+ * 1.) Create a global state with active/inactive, blocked site list, etc. Use Background.js?
+ * 2.) Create a wrapper for the onChanged events, where I can listen to each change individually. Would still need to maintain some kind of global state.
+ * As-is, this is pretty hacky.
+ */
 chrome.storage.onChanged.addListener(changes => {
-    if (changes.isExtensionActive) {
-        handleToggleEvent(changes.isExtensionActive.newValue);
-    }
+    getStorageData('isExtensionActive').then((isExtensionActive) => {
+        if (isExtensionActive) {
+            blockWebsite(isExtensionActive);
+        }
+    });
 });
 
-function handleToggleEvent(state) {
-    console.log('new state:', state);
+// todo: revisit this
+function blockWebsite(isActive) {
+    getStorageData('blocklist').then((blocklist) => {
+        if (isActive) {
+            let hostname = new URL(window.location.href).hostname;
+            blocklist = blocklist || new Array(); // todo: remove this hack
+            hostname = hostname.replace('www.', ''); // todo: remove this hack
+            if (blocklist.includes(hostname)) {
+                location.replace(`chrome-extension://${chrome.runtime.id}/blocked-page/blocked-page.html`);
+            }
+        } else {
+            // todo: remove event listeners, if necessary
+        }
+    });
+}
+
+function getStorageData(key) {
+    return new Promise((resolve) => {
+        chrome.storage.sync.get(key, storage => {
+            resolve(storage[key]);
+        });
+    });
 }
